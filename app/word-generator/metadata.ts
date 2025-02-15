0b4f59fcd832e2c07ee2df2618bee8aad60b9a51
maaku saito <abokadoaoinaaiueo37@gmail.com>
2025-02-15 11:48:24 +0900

    ワード自動生成ツールの実装

---
+++ app/word-generator/metadata.ts
@@ -0,0 +1,23 @@
+const jsonLdData = {
+  "@context": "https://schema.org",
+  "@type": "WebApplication",
+  "name": "Random Word Generator - Boring Tools",
+  "description": "Generate random words instantly from our curated list of English words. Perfect for creative writing, brainstorming, vocabulary building, and more. Free and easy to use!",
+  "url": "https://boring-tool.com/word-generator",
+  "applicationCategory": "UtilityApplication",
+  "operatingSystem": "Any",
+  "offers": {
+    "@type": "Offer",
+    "price": "0",
+    "priceCurrency": "USD"
+  },
+  "featureList": [
+    "Generate 1-100 random words",
+    "Instant word generation",
+    "Copy to clipboard functionality",
+    "Free to use",
+    "No registration required"
+  ]
+};
+
+export default jsonLdData; 
