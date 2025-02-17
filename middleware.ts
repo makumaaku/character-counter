50727e8d9ddd38957cfa48fb71894472ecf3f8a7
maaku saito <abokadoaoinaaiueo37@gmail.com>
2025-02-17 23:42:00 +0900

    リダイレクト設定の修正。TOP画面は多言語対応しない。

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,9 @@
-import { match } from '@formatjs/intl-localematcher';
-import Negotiator from 'negotiator';
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 const locales = ['en', 'ja'];
 const defaultLocale = 'en';
-
-function getLocale(request: NextRequest): string {
-  const negotiatorHeaders: Record<string, string> = {};
-  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
-
-  const languages = new Negotiator({ headers: negotiatorHeaders }).languages();
-  return match(languages, locales, defaultLocale);
-}
+const rootPath = '/';
 
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
@@ -20,10 +11,13 @@ export function middleware(request: NextRequest) {
     (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
   );
 
+  if(pathname === rootPath) {
+    return;
+  }
+
   // Redirect if there is no locale
   if (pathnameIsMissingLocale) {
-    const locale = getLocale(request);
-    request.nextUrl.pathname = `/${locale}${pathname}`;
+    request.nextUrl.pathname = `/${defaultLocale}${pathname}`;
     return NextResponse.redirect(request.nextUrl);
   }
 }
