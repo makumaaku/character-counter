5f63d2e17334c7d9a017b12fc8c1f8cc1e555553
maaku saito <abokadoaoinaaiueo37@gmail.com>
2025-02-27 04:10:19 +0900

    数独の文字列修正

--- app/page.tsx
+++ app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
               <Link href="/word-gen/story-generator" className="text-white hover:underline ml-4">Random Story Generator</Link>
             </li>
             <li>
-              <Link href="/number-quiz" className="text-white hover:underline">Number Quiz Game</Link>
+              <Link href="/number-quiz" className="text-white hover:underline">Number Place</Link>
             </li>
             <li>
               <Link href="/link-status-checker" className="text-white hover:underline">Link Status Checker</Link>
