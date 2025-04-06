69f666b138350f7e887243a8bceb3fa0e6482896
maaku saito <abokadoaoinaaiueo37@gmail.com>
2025-04-06 18:22:14 +0900

    ヘッダーのタイトルをセンタリングするように

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,19 +11,21 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ title, homeLink, isH1 = false, children }) => {
   return (
-    <header className="bg-primary text-white flex justify-between items-center p-4">
-      <div className="flex items-center gap-2">
+    <header className="bg-primary text-white flex items-center p-4">
+      <div className="flex-1 flex items-center gap-2">
         {children}
       </div>
-      <Link href={homeLink}>
+      <Link href={homeLink} className="flex-1 flex justify-center">
         {isH1 ? (
           <h1 className="text-sm md:text-xl font-bold hover:text-gray-200 transition-colors cursor-pointer">{title}</h1>
         ) : (
           <div className="text-sm md:text-xl font-bold hover:text-gray-200 transition-colors cursor-pointer">{title}</div>
         )}
       </Link>
-      <div className="w-[90px] flex items-center justify-end ml-4">
-        <LanguageSwitcher />
+      <div className="flex-1 flex items-center justify-end">
+        <div className="w-[90px]">
+          <LanguageSwitcher />
+        </div>
       </div>
     </header>
   );
